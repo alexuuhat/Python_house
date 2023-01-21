d5f7567e88c16bdf78bad80956ac188b085f85b0
Alex <81509258+alexuuhat@users.noreply.github.com>
2023-01-21 15:41:00 +0530

    Desktop Notification

---
+++ DesktopNotification.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+if __name__=="__main__":
+    while True:
+        notification.notify(
+            title = "Alert!!",
+            message = "Take a break. It is been an hour!",
+            timeout = 10
+        )
+        time.sleep(3600)
